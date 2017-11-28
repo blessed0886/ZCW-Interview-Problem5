d5f3feb4979dd622bda2ef1a34015099048a8996
donald f <donaldf@zipcoders-MacBook-Pro-9.local>
2017-11-28 10:40:06 -0500

    never finished, had no clue how

--- src/main/java/io/zipcoder/Problem5.java
+++ src/main/java/io/zipcoder/Problem5.java
@@ -1,4 +1,28 @@
 package io.zipcoder;
 
 public class Problem5 {
+
+
+        public String reverseString(String rawData){
+            for(int i = rawData.length()-1; i == 0; i--){
+                rawData.replace(rawData.charAt(i), rawData.charAt()).replace(rawData.charAt(), rawData.charAt());
+            }
+                return rawData;
+        }
+
+        public String reverseSrtingWhile(String rawData){
+            while(){
+
+            }
+            return rawData;
+        }
+
+
+        //This is the easy way why do I have to do it without creating new variables or using ANY built in functions
+        //Or using google for a hint to do it without usable criteria
+        public String easyReverse(String rawData){
+            StringBuilder builder = new StringBuilder(rawData);
+            return builder.reverse().toString();
+        }
+
 }
